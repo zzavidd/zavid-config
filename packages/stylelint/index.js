a58d12e62e40fa96783e55f2d899faf84fbcc7e4
Zavid <zavidegbue@gmail.com>
2021-02-28 16:30:50 +0000

    Add BEM selector class pattern

--- packages/stylelint/index.js
+++ packages/stylelint/index.js
@@ -2,6 +2,8 @@ module.exports = {
   extends: 'stylelint-config-sass-guidelines',
   rules: {
     'max-nesting-depth': 5,
+    'selector-class-pattern': "^[a-z]+(?:-[a-z]+)*(?:__[a-z]*)?(?:--[a-z]*)?$",
     'selector-max-compound-selectors': 5,
+    'selector-max-id': 1
   },
 };
