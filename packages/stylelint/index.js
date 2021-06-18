9fb4b73ef6decfd252891567a7c00e2bc4b491c7
Zavid <zavidegbue@gmail.com>
2021-06-18 22:13:14 +0100

    Ignore selector-no-qualifying-type stylelint error

--- packages/stylelint/index.js
+++ packages/stylelint/index.js
@@ -6,6 +6,10 @@ module.exports = {
     'max-nesting-depth': 5,
     'selector-class-pattern': selectorRegex,
     'selector-max-compound-selectors': 5,
-    'selector-max-id': 1
+    'selector-max-id': 1,
+    'selector-no-qualifying-type': [
+      true,
+      { ignore: ['attribute', 'class', 'id'] },
+    ],
   },
 };
