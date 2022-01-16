02c71b939c3b9e44ef76348c963886adb745ae83
Zavid <zavidegbue@gmail.com>
2022-01-16 23:39:20 +0000

    Update prettier config

--- prettier/index.js
+++ prettier/index.js
@@ -1,11 +1,13 @@
+/** @type {import('prettier').Config} */
 module.exports = {
   arrowParens: 'always',
   bracketSpacing: true,
   jsxBracketSameLine: true,
   jsxSingleQuote: false,
+  printWidth: 80,
   semi: true,
   singleQuote: true,
   tabWidth: 2,
-  trailingComma: 'none',
+  trailingComma: 'es5',
   useTabs: false,
 };
