7fe72fb75ffe9354668acbbaf459567c372666f0
Zavid <zavidegbue@gmail.com>
2022-05-20 00:14:30 +0100

    Tidy tsconfig

--- test/pages/_app.tsx
+++ test/pages/_app.tsx
@@ -1,8 +1,9 @@
 import type { AppProps } from 'next/app';
+import type { ReactElement } from 'react';
 import React from 'react';
 
-import '../styles/App.scss';
+import 'styles/App.scss';
 
-export default function TestApp({ Component, pageProps }: AppProps) {
+export default function TestApp({ Component, pageProps }: AppProps): ReactElement {
   return <Component {...pageProps} />;
 }
