af8e22df280f8d99cb8ec7133cd8b1a694f5ef50
Zavid <zavidegbue@gmail.com>
2022-05-20 00:10:24 +0100

    Remove stylelint variants

--- stylelint/index.js
+++ stylelint/index.js
@@ -1,9 +1,13 @@
+const SELECTOR_PATTERN =
+  '^[a-z]+(?:-[a-z]+)*(?:__[a-z]+(?:-[a-z]+)*)?(?:--[a-z]+(?:-[a-z]+)*)?$';
+
 /**
  * @type {import('stylelint').ConfigRules}
  */
 module.exports = {
   'function-parentheses-space-inside': 'never-single-line',
   'max-nesting-depth': 5,
+  'selector-class-pattern': SELECTOR_PATTERN,
   'selector-max-compound-selectors': 5,
   'selector-max-id': 1,
   'selector-no-qualifying-type': [
--- test/.stylelintrc.js
+++
@@ -1,6 +0,0 @@
-/**
- * @type {import('stylelint').Config}
- */
-module.exports = {
-  extends: '@zzavidd/stylelint-config/sc'
-};
